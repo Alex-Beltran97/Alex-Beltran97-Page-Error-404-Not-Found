bbaaaa9c7d0cb55bdc169933439578cb69b19795
Alex-Beltran97 <92172762+Alex-Beltran97@users.noreply.github.com>
2021-12-06 19:22:26 -0500

    Se aniadio una seudo clase al boton, active

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ new Vue ({
 	el:"#app",
 	data(){
 		return{
-			title_error:"404 Not Found"
+			title_error:"404 Not Found",
+			title_error_messages:"Opps! Something went Wrong.",
+			error_messages:"The page you are looking for might be removed or is temporaly unavalible.",
+			button_message:"Back to home page",
+			img:"img/Technical difficulties.jpg",
+		}
+	},
+	methods:{
+		prueba(){
+			// this.value = !this.value;
+			alert("holo");
 		}
 	}
 })
